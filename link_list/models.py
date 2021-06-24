a0575288c59af98341c759fa47af88bc3351c8d5
joisemp <joise1124@gmail.com>
2021-06-24 22:39:02 +0530

    created model, view, template and url for link list

--- link_list/models.py
+++ link_list/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Link(models.Model):
+    name = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    image = models.CharField(max_length=256, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
--- link_list/views.py
+++ link_list/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Link
 
-# Create your views here.
+class LinksListView(ListView):
+    template_name = 'link_list/index.html'
+    queryset = Link.objects.all()
+    context_object_name = 'links'
